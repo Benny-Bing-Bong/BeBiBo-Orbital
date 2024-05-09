f8c7b09b4769fc21b75b309adccaaf100ed2561c
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-09 20:21:32 +0800

    making prev commit clearer by adding vars

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -5,7 +5,6 @@ class_name Player
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var sword: Area2D = $Sword
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction: Vector2 = Vector2.ZERO
@@ -31,9 +30,9 @@ func update_animation_parameters() -> void:
 	animation_tree.set("parameters/move/blend_position", direction.x)
 
 func update_facing_direction() -> void:
-	var sword_col_shape: CollisionShape2D = sword.get_node("CollisionShape2D")
-	var dist: float = abs($CollisionShape2D.position.x - sword_col_shape.position.x)
-	print(dist)
+	var sword_col_shape: CollisionShape2D = $Sword/CollisionShape2D
+	var player_col_shape: CollisionShape2D = $CollisionShape2D
+	var dist: float = abs(player_col_shape.position.x - sword_col_shape.position.x)
 	
 	if direction.x > 0:
 		sprite.flip_h = false
