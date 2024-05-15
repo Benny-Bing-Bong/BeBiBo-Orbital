f77e6f1aef10d442f0768d7cdd8942102e0385a5
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-15 23:55:53 +0800

    Hotfix to prevent stuck, but bug still happens

--- Characters/Player/state_attack.gd
+++ Characters/Player/state_attack.gd
@@ -3,6 +3,11 @@ extends State
 @export var atk2_anim_name: String
 
 @onready var dbl_atk_timer: Timer = $DblAtkTimer
+@onready var timer: Timer = $Timer
+
+func enter() -> void:
+	super()
+	timer.start();
 
 func state_input(_input: InputEvent) -> void:
 	if _input.is_action_pressed("attack"):
@@ -16,3 +21,6 @@ func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 			playback.travel(atk2_anim_name)
 	if anim_name == atk2_anim_name:
 		transitioned.emit(self, "ground")
+
+func _on_timer_timeout() -> void:
+	transitioned.emit(self, "ground")
