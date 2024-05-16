b45c7a954120ab77b026eea7a8c118d4ce612162
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-16 18:27:11 +0800

    Add Gravity Node to player
    Removed sprite export var from gravity as well since its not used

--- States and Abstracts/gravity.gd
+++ States and Abstracts/gravity.gd
@@ -2,12 +2,11 @@ class_name Gravity
 extends Node
 
 @export var character_body : CharacterBody2D
-@export var sprite : AnimatedSprite2D
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta : float) -> void:
 	if not character_body.is_on_floor():
 		character_body.velocity.y += gravity * delta
-		
+	
 	character_body.move_and_slide()
