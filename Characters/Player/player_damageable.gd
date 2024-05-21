9fbffc26535d257982d467a97236721cff2c9e14
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-21 14:49:58 +0800

    Edited display viewport settings
    Also updated camera zooms back to 1x to make sure changes are reflected properly

--- Characters/Player/player_damageable.gd
+++ Characters/Player/player_damageable.gd
@@ -4,3 +4,4 @@ func _die() -> void:
 	for child in get_parent().get_children():
 		if child is PlayerStateMachine:
 			child.emit_signal("interrupted_state", "death")
+	print("Player has died. Press P to restart scene")
