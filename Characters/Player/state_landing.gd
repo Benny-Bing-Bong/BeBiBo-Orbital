caf5afed86680693d686a3b2a4cf4fb51f6b36e5
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-23 00:11:43 +0800

    Decoupling state transitions and checks

--- Characters/Player/state_landing.gd
+++ Characters/Player/state_landing.gd
@@ -2,4 +2,7 @@ extends State
 
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if anim_name == first_anim_name:
-		transitioned.emit(self, "ground")
+		land()
+
+func land() -> void:
+	transitioned.emit(self, "ground")
