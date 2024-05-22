7bb8ab0b62c7abf1966d7dcdc4ef55ecbe4971f1
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-23 00:15:28 +0800

    Decouple checks and player state transitions

--- Characters/Player/state_attack.gd
+++ Characters/Player/state_attack.gd
@@ -16,11 +16,14 @@ func state_input(_input: InputEvent) -> void:
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if anim_name == first_anim_name:
 		if dbl_atk_timer.is_stopped():
-			transitioned.emit(self, "ground")
+			go_back_move()
 		else:
 			playback.travel(atk2_anim_name)
 	if anim_name == atk2_anim_name:
-		transitioned.emit(self, "ground")
+		go_back_move()
 
 func _on_timer_timeout() -> void:
+	transitioned.emit(self, "ground")
+
+func  go_back_move() -> void:
 	transitioned.emit(self, "ground")
--- Characters/Player/state_falling.gd
+++ Characters/Player/state_falling.gd
@@ -2,11 +2,14 @@ extends State
 
 func state_process(_delta: float) -> void:
 	if character.is_on_floor():
-		transitioned.emit(self, "ground")
+		land()
 
 func state_input(_input: InputEvent) -> void:
 	if _input.is_action_pressed("up"):
 		jump()
+
+func land() -> void:
+	transitioned.emit(self, "ground")
 
 func jump() -> void:
 	transitioned.emit(self, "air")
