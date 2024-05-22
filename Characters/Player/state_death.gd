0c919c7a28e6296120cb1e5d61b7a1b424c2dc7e
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-22 09:58:03 +0800

    Add HUB to TL2 and update player death
    Make it such that player velocity becomes 0 when they die

--- Characters/Player/state_death.gd
+++ Characters/Player/state_death.gd
@@ -3,3 +3,7 @@ extends State
 func enter() -> void:
 	super()
 	GameManager.player_died()
+
+func state_physics_process(_delta: float) -> void:
+	character.velocity.x = 0
+	character.move_and_slide()
