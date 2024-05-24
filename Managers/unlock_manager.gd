f16c6d79235d962b115c8f84b5772cc627cfbda7
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-24 11:08:23 +0800

    Manager and Resource Setup
    Setting up managers and resources for feature

---
+++ Managers/unlock_manager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+# the values here are the defaults, set when player opens the game for the
+#first time
+var unlock_dictionary: Dictionary = {
+	"jump": false,
+	"double_jump": false,
+	"crouch": false,
+	"wallhang": false,
+}
+
+# check if can do given action, and returns it. Default value if not found
+#is false
+func able_to(action: String) -> bool:
+	return unlock_dictionary.get(action, false)
+
+# first check if the key is being tracked by the dictionary. if it is,
+#set its value to true
+func unlock(action: String) -> void:
+	if unlock_dictionary.has(action):
+		unlock_dictionary[action] = true
