1084aca3d236707a18af0748fa8f50372a0f66ed
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-29 17:04:12 +0800

    Finished Level 1-2, starting 1-3

--- Managers/scene_manager.gd
+++ Managers/scene_manager.gd
@@ -6,6 +6,7 @@ var scenes: Dictionary = {
 	"TestLevel2": "res://Levels/Test Level/test_level_2.tscn",
 	"Level1-1": "res://Levels/Actual Game Levels/level_1-1.tscn",
 	"Level1-2": "res://Levels/Actual Game Levels/level_1-2.tscn",
+	"Level1-3": "res://Levels/Actual Game Levels/level_1-3.tscn"
 }
 
 func transition_to_scene(scene_name: String) -> void:
