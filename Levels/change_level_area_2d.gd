adaf5bb4a3355887e4f4b31cf0fe8825f72599d1
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-05-29 14:05:39 +0800

    Change scene change system

--- Levels/change_level_area_2d.gd
+++ Levels/change_level_area_2d.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-@export var new_level_path: String
+@export var new_level_name: String
 
 # because of collision mask, no need to explicitly check for player
 func _on_body_entered(body: Node2D) -> void:
-	SceneManager.transition_to_scene(new_level_path)
+	SceneManager.transition_to_scene(new_level_name)
--- Managers/scene_manager.gd
+++ Managers/scene_manager.gd
@@ -1,7 +1,16 @@
 extends Node
 
-func transition_to_scene(scene_path: String) -> void:
-	var err: int = get_tree().change_scene_to_file(scene_path)
+var scenes: Dictionary = {
+	"Level1": "res://Levels/level_1.tscn",
+	"TestLevel1": "res://Levels/Test Level/test_level.tscn",
+	"TestLevel2": "res://Levels/Test Level/test_level_2.tscn",
+}
+
+func transition_to_scene(scene_name: String) -> void:
+	var scene_path: String = scenes.get(scene_name)
+	if scene_path == null:
+		return
 	
+	var err: int = get_tree().change_scene_to_file(scene_path)
 	if not err == OK:
 		print("Error: Unable to change scene")
