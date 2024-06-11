8e41fc5d954a3825e195e35138c6919d7f2f356d
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-06-11 16:23:30 +0800

    Removed restart and reset keybinds
    As there is no need for them anymore. These functions are part of the menus now

--- Managers/game_manager.gd
+++ Managers/game_manager.gd
@@ -7,20 +7,6 @@ var on_main_screen: bool = false
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color(0.97, 0.74, 0.66, 1.00))
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("restart"):
-		pass
-		#player_alive = true
-		#HealthManager.set_health(3)
-		#SceneManager.transition_to_scene("Level1-1")
-	
-	if event.is_action_pressed("reset"):
-		pass
-		#player_alive = true
-		#HealthManager.set_health(3)
-		#UnlockManager.reset_unlocks()
-		#SceneManager.transition_to_scene("Level1-1")
-
 # save the game on quit (by clicking X at top right)
 func _notification(what: int) -> void:
 	if what == Node.NOTIFICATION_WM_CLOSE_REQUEST and not on_main_screen:
