7714af614d96905e2323d647a2b2222185247a7d
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-06-17 16:27:13 +0800

    Fixed Attack Bug FINALLY
    Did it by removing the signal to go back to move being based on the animation finishing, and changing it to a timer instead. Separated both attacks

--- Characters/Player/state_attack.gd
+++ Characters/Player/state_attack.gd
@@ -1,41 +1,41 @@
 extends State
 
-@export var atk2_anim_name: String
-@export var attack_speed: int = 1500
-
-@onready var dbl_atk_timer: Timer = $DblAtkTimer
 @onready var timer: Timer = $Timer
-@onready var direction: Vector2
-@onready var gravity: Gravity = $"../../Gravity"
+@onready var dbl_atk_timer: Timer = $DblAtkTimer
 
 func enter() -> void:
 	super()
-	timer.start();
-	
+	timer.start()
+
+func state_input(_input: InputEvent) -> void:
+	if _input.is_action_pressed("attack"):
+		dbl_atk_timer.start()
+
+func _on_timer_timeout() -> void:
+	if dbl_atk_timer.is_stopped():
+		go_back_move()
+	else:
+		double_attack()
+
+func double_attack() -> void:
+	transitioned.emit(self, "attack2")
+
+func go_back_move() -> void:
+	transitioned.emit(self, "ground")
+
+# FOR AIR ATTACK SAVE HERE FIRST
+#@export var attack_speed: int = 1500
+#@onready var direction: Vector2
+#@onready var gravity: Gravity = $"../../Gravity"
+# in enter
 	#gravity.disable()
 	#character.velocity.y = 0
 	
 	#direction = character.get_facing_direction()
 	#character.velocity.x = direction.x * attack_speed
 
-func state_input(_input: InputEvent) -> void:
-	if _input.is_action_pressed("attack"):
-		dbl_atk_timer.start()
-		#character.velocity.x = direction.x * attack_speed
+# in state_input for moving forward while attacking
+	#character.velocity.x = direction.x * attack_speed	
 
-func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
-	if anim_name == first_anim_name:
-		if dbl_atk_timer.is_stopped():
-			go_back_move()
-		else:
-			playback.travel(atk2_anim_name)
-	if anim_name == atk2_anim_name:
-		go_back_move()
-
-func _on_timer_timeout() -> void:
+# in go_back_move
 	#gravity.enable()
-	transitioned.emit(self, "ground")
-
-func go_back_move() -> void:
-	#gravity.enable()
-	transitioned.emit(self, "ground")
