117429fe083c91a1da101797b0a0328418d3f023
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-07-19 13:50:06 +0800

    Go back main after ending

--- Audio/SFX/CutsceneSFX/cutscene_sfx_manager.gd
+++ Audio/SFX/CutsceneSFX/cutscene_sfx_manager.gd
@@ -5,7 +5,7 @@ const GOOD_END_SFX = preload("res://Audio/SFX/CutsceneSFX/good_end_sfx.wav")
 const PULSE_SFX = preload("res://Audio/SFX/CutsceneSFX/pulse_sfx.wav")
 
 func pulse() -> void:
-	play_sfx(PULSE_SFX, 0).play()
+	play_sfx(PULSE_SFX, -5).play()
 
 func good_end() -> void:
 	play_sfx(GOOD_END_SFX, -5).play()
