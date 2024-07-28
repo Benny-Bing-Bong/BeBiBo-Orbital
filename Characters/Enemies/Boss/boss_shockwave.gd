b2b2238486bcbd7dcea21a500844694c8ce34754
dsyc <62531591+deseansoh@users.noreply.github.com>
2024-07-28 14:24:25 +0800

    Boss balancing

--- Characters/Enemies/Boss/boss_shockwave.gd
+++ Characters/Enemies/Boss/boss_shockwave.gd
@@ -2,7 +2,7 @@ class_name BossShockwave
 extends RigidBody2D
 
 @export var animated_sprite_2d: AnimatedSprite2D
-var shockwave_speed: int = 500
+var shockwave_speed: int = 350
 
 func _physics_process(delta: float) -> void:
 	linear_velocity.x = shockwave_speed
